63c048eb24113238ffc4a7debe9b8e8a15b14b97
LuooChen <jiu-mu@outlook.com>
2022-03-23 14:23:06 +0000

    feat: add configs

--- my_scripts/train/my_train.sh
+++ my_scripts/train/my_train.sh
@@ -1,1 +1,1 @@
-python tools/train.py configs/vgg/my_vgg16_b16_upper_colors_11.py --work-dir work_dirs/my_vgg16_b16_upper_colors_11
+python tools/train.py configs/vgg/my_vgg16_b16_lower_colors_11.py --work-dir work_dirs/my_vgg16_b16_lower_colors_11
