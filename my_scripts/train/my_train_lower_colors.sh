99891877feea5a3ff2d56391a50b850dbbfaca20
LuooChen <jiu-mu@outlook.com>
2022-03-28 19:54:10 +0000

    feat: add configs

--- my_scripts/train/my_train_lower_colors.sh
+++ my_scripts/train/my_train_lower_colors.sh
@@ -1,1 +1,1 @@
-python tools/train.py configs/vgg/my_vgg19bn_b16_lower_colors_14.py --work-dir work_dirs/my_vgg19bn_b16_lower_colors_14
+python tools/train.py configs/vgg/my_vgg19bn_b16_lower_colors_15.py --work-dir work_dirs/my_vgg19bn_b16_lower_colors_15
--- my_scripts/train/my_train_main_7_props.sh
+++ my_scripts/train/my_train_main_7_props.sh
@@ -1,1 +1,1 @@
-python tools/train.py configs/vgg/my_vgg19bn_b16_main_7_props_14.py --work-dir work_dirs/my_vgg19bn_b16_main_7_props_14
+python tools/train.py configs/vgg/my_vgg19bn_b16_main_7_props_15.py --work-dir work_dirs/my_vgg19bn_b16_main_7_props_15
