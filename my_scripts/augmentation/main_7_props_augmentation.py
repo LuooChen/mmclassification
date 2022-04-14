b98bb0657091b5c53256425045d6e13c67a15143
luoochen <jiu-mu@outlook.com>
2022-04-14 17:13:08 +0000

    feat: update augmentation

--- my_scripts/augmentation/main_7_props_augmentation.py
+++ my_scripts/augmentation/main_7_props_augmentation.py
@@ -163,50 +163,63 @@ def noisy(img, noise_type="gauss"):
 def join_image_path(img_name, aug_name, suffix):
     return main_7_props_augmentation_image_prefix + img_name + '_' + aug_name + '.' + suffix
 
-def img_aumentation(img_src, img_full_name, prop) -> list:
-    # img_full_name = img_src.split('/')[1]
-    img_name = img_full_name.split('.')[0]
-    img_suffix = img_full_name.split('.')[1]
+def save_image(org_img_info, img_path_name, _img, is_flip=False):
+    aug_img_info = copy.deepcopy(org_img_info)
+    aug_img_info['name'] = img_path_name
+    if is_flip:
+        if aug_img_info['towards'] == 'left':
+            aug_img_info['towards'] = 'right'
+        elif aug_img_info['towards'] == 'right':
+            aug_img_info['towards'] = 'left'
+    # save image
+    cv2.imwrite(os.path.join(src_image_path, img_path_name), _img)
+    # cv2.imwrite(image_dst, image, [cv2.IMWRITE_JPEG_QUALITY, 100])
+    return aug_img_info
+
+def img_aumentation(img_src, src_img_full_name, prop, _row):
+    img_name = src_img_full_name.split('.')[0]
+    img_suffix = src_img_full_name.split('.')[1]
     img = cv2.imread(img_src)
+    imgs_obj = {
+        src_img_full_name: [img, _row]
+    }
     img_flip = horizontal_flip(img)
-    augmentation_images = {}
-    augmentation_images[join_image_path(img_name, 'flip', img_suffix)] = img_flip
-    img_list = [img, img_flip]
+    flip_img_full_name = join_image_path(img_name, 'flip', img_suffix)
+    aug_img_info_flip = save_image(_row, flip_img_full_name, img_flip, True)
+    imgs_obj[flip_img_full_name] = [img_flip, aug_img_info_flip]
     # rotation
     if prop in allow_rotation:
+        temp_image_full_name_list = [x for x in imgs_obj.keys()]
         for angle in rotation_angles[prop]:
             index_count = 0
-            temp_list = copy.deepcopy(img_list)
-            for _img in temp_list:
-                img_rotation = rotation(_img, angle)
-                img_list.append(img_rotation)
-                augmentation_images[join_image_path(img_name, str(angle) + 'angle' + str(index_count), img_suffix)] = img_rotation
-                img_rotation_reverse = rotation(_img, -angle)
-                img_list.append(img_rotation_reverse)
-                augmentation_images[join_image_path(img_name, str(angle)  + 'angle' + 'reverse' + str(index_count), img_suffix)] = img_rotation_reverse
+            for _img_full_name in temp_image_full_name_list:
+                _img, _img_info = imgs_obj[_img_full_name]
+                rotation_img = rotation(_img, angle)
+                rotation_img_full_name = join_image_path(img_name, str(angle) + 'angle' + str(index_count), img_suffix)
+                rotation_img_info = save_image(_img_info, rotation_img_full_name, rotation_img)
+                imgs_obj[rotation_img_full_name] = [rotation_img, rotation_img_info]
+
+                rotation_reverse_img = rotation(_img, -angle)
+                rotation_reverse_img_full_name = join_image_path(img_name, str(angle)  + 'angle' + 'reverse' + str(index_count), img_suffix)
+                rotation_reverse_info = save_image(_img_info, rotation_reverse_img_full_name, rotation_reverse_img)
+                imgs_obj[rotation_reverse_img_full_name] = [rotation_reverse_img, rotation_reverse_info]
                 index_count = index_count+1
     # color jitter
     if prop in allow_color_jitters:
+        temp_image_full_name_list = [x for x in imgs_obj.keys()]
         for _type in color_jitters[prop]:
             color_count = 0
-            temp_list = copy.deepcopy(img_list)
-            for _img in temp_list:
-                _color_jitter_img = colorjitter(_img, _type)
-                img_list.append(_color_jitter_img)
-                augmentation_images[join_image_path(img_name, 'colorjitter'+_type+str(color_count), img_suffix)] = _color_jitter_img
+            for _img_full_name in temp_image_full_name_list:
+                _img, _img_info = imgs_obj[_img_full_name]
+                color_jitter_img = colorjitter(_img, _type)
+                color_jitter_img_full_name = join_image_path(img_name, 'colorjitter'+_type+str(color_count), img_suffix)
+                color_jitter_img_info = save_image(_img_info, color_jitter_img_full_name, color_jitter_img)
+                imgs_obj[color_jitter_img_full_name] = [color_jitter_img, color_jitter_img_info]
                 color_count = color_count+1
-    return augmentation_images
-
-def save_augmentation_imgs(org_img_info, augmentation_images) -> list:
     augmentation_img_infos = []
-    for img_path_name in augmentation_images:
-        aug_img_info = copy.deepcopy(org_img_info)
-        aug_img_info['name'] = img_path_name
-        # save image
-        cv2.imwrite(os.path.join(src_image_path, img_path_name), augmentation_images[img_path_name])
-        # cv2.imwrite(image_dst, image, [cv2.IMWRITE_JPEG_QUALITY, 100])
-        # add label
-        augmentation_img_infos.append(aug_img_info)
+    for _img_full_name in imgs_obj:
+        if _img_full_name != src_img_full_name:
+            augmentation_img_infos.append(imgs_obj[_img_full_name][1])
     return augmentation_img_infos
 
 def generate_csv_file(data_info, filepath):
@@ -222,10 +235,9 @@ if __name__ == "__main__":
     for _prop in allow_augmentation_data_info:
         for _row in allow_augmentation_data_info[_prop]:
             img_path_name = _row['name']
-            img_full_name = img_path_name.split('/')[1]
-            augmentation_images = img_aumentation(os.path.join(src_image_path, img_path_name), img_full_name, _prop)
-            augmentation_img_infos = save_augmentation_imgs(_row, augmentation_images)
+            src_img_full_name = img_path_name.split('/')[1]
+            augmentation_img_infos = img_aumentation(os.path.join(src_image_path, img_path_name), src_img_full_name, _prop, _row)
             all_augmentation_data_info = all_augmentation_data_info + augmentation_img_infos
     # write labels
     generate_csv_file(all_augmentation_data_info, main_7_props_augmentation_labels)
-    print("rewrite successfully.")
+    print("augmentation successfully.")
